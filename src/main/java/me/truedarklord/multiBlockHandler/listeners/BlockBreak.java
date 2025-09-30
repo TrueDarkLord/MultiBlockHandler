ae3b5cb79a19a195c7e21919b52a78fabd934d27
TrueDarkLord <truedarklord.email@gmail.com>
2025-09-30 11:01:09 -0600

    Multiblock Structure Support (#2)
    * Bidirectional
    
    Have a whitelist for both blocks that go up and ones that go down.
    
    * Update Multi-Block Logic
    
    Gather all drops prior to breaking the blocks due to multiblock structures breaking when other blocks break.
    Set all blocks to air in reverse to ensure that multiblock structures are removed prior to the blocks that they are on.
    
    * Handle Berries
    
    Create a method to pop berries from their plant.
    
    * Fix Berry Handling
    
    Due to the sequence of events, all of the drop need to be handled within the initial block break event.
    
    * Version Bump

--- src/main/java/me/truedarklord/multiBlockHandler/listeners/BlockBreak.java
+++ src/main/java/me/truedarklord/multiBlockHandler/listeners/BlockBreak.java
@@ -1,15 +1,22 @@
 package me.truedarklord.multiBlockHandler.listeners;
 
 import me.truedarklord.multiBlockHandler.MultiBlockHandler;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.type.CaveVinesPlant;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BlockBreak implements Listener {
@@ -24,22 +31,85 @@ public class BlockBreak implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onBreak(BlockDropItemEvent event) {
+        if (!(event.getBlock().getBlockData() instanceof CaveVinesPlant)) return;
+
         Material type = event.getBlockState().getType();
-        Block currentBlock = event.getBlock().getRelative(0, 1, 0);
+        FileConfiguration config = plugin.getConfig();
+        Block currentBlock;
+        List<Block> blocks = new ArrayList<>();
         List<Item> drops = event.getItems();
+        int offset = 0;
 
-        if (!plugin.getConfig().getStringList("whitelist").contains(type.toString())) return;
+        if (config.getStringList("above-whitelist").contains(type.toString())) offset = 1;
+        if (config.getStringList("below-whitelist").contains(type.toString())) offset = -1;
+        currentBlock = event.getBlock().getRelative(0, offset, 0);
 
-        while (currentBlock.getType().equals(type)) {
+        if (offset == 0) return;
+
+        List<String> byproduct = config.getStringList("byproduct." + type);
+
+        while (currentBlock.getType().equals(type) || byproduct.contains(currentBlock.getType().toString())) {
 
             for (ItemStack drop : currentBlock.getDrops()) {
                 drops.add(currentBlock.getWorld().dropItemNaturally(currentBlock.getLocation(), drop));
             }
 
-            currentBlock.setType(Material.AIR);
-            currentBlock = currentBlock.getRelative(0, 1, 0);
+            blocks.add(currentBlock);
+            currentBlock = currentBlock.getRelative(0, offset, 0);
         }
 
+        /*
+            Due to how multi-block structures work:
+              All drops need to be captured prior to setting blocks to air.
+              Setting blocks to air needs to be reversed.
+         */
+        for (int i = blocks.size() - 1; i >= 0; i--) {
+            blocks.get(i).setType(Material.AIR);
+        }
+
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    public void onBreak(BlockBreakEvent event) {
+        if (!(event.isDropItems())) return;
+        if (!(event.getBlock().getBlockData() instanceof CaveVinesPlant)) return;
+
+        Block firstBlock = event.getBlock().getRelative(0, -1, 0);
+        Block currentBlock = firstBlock;
+        List<Item> drops = new ArrayList<>();
+        List<Block> blocks = new ArrayList<>();
+
+        while (popBerries(currentBlock, drops)) {
+            blocks.add(currentBlock);
+            currentBlock = currentBlock.getRelative(0, -1, 0);
+        }
+
+        BlockDropItemEvent dropEvent = new BlockDropItemEvent(firstBlock, firstBlock.getState(), event.getPlayer(), drops);
+        Bukkit.getPluginManager().callEvent(dropEvent);
+
+        if (dropEvent.isCancelled()) drops.forEach(Entity::remove);
+
+        blocks.forEach(block -> block.setType(Material.AIR));
+
+    }
+
+    /**
+     * Gathers and stores berries in the drops of an item.
+     * @param block The block to check for berries.
+     * @param drops The list of drops to add the item to.
+     * @return True if the block is a CaveVine.
+     */
+    private boolean popBerries(Block block, List<Item> drops) {
+        if (!(block.getBlockData() instanceof CaveVinesPlant plant)) return false;
+
+        if (!plant.isBerries()) return true;
+
+        plant.setBerries(false);
+        drops.add(block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(Material.GLOW_BERRIES)));
+
+        block.setBlockData(plant);
+
+        return true;
     }
 
 }
